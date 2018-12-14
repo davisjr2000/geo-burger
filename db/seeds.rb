3b3d5a052de52931fdd1432ad88ba7b0c006e042
Davis JR <davisjr2000@hotmail.com>
2018-12-13 23:49:07 -0200

    Improve Home Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ if cleandatabase
   BurgerPlace.delete_all
 end
 
-BurgerPlace.create(address:"24 Alameda Santos, Brasil", name: "A Chapa")
-BurgerPlace.create(address:"Rua Bandeira Paulista, 164 Brasil", name: "Butchers Market")
+BurgerPlace.create(address:"24 Alameda Santos, Brasil", name: "A Chapa", rating: 5)
+BurgerPlace.create(address:"Rua Bandeira Paulista, 164 Brasil", name: "Butchers Market",rating: 4)

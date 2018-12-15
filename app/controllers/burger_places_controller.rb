6a6a7de39e29075e001904f7d53be47ba2c42597
Davis JR <davisjr2000@hotmail.com>
2018-12-15 20:17:56 -0200

    Add pop-ups

--- app/controllers/burger_places_controller.rb
+++ app/controllers/burger_places_controller.rb
@@ -3,10 +3,11 @@ class BurgerPlacesController < ApplicationController
   def index
     @burger_places = BurgerPlace.where.not(latitude: nil, longitude: nil)
 
-    @markers = @burger_places.map do |flat|
+    @markers = @burger_places.map do |burger_place|
       {
-        lng: flat.longitude,
-        lat: flat.latitude
+        lng: burger_place.longitude,
+        lat: burger_place.latitude,
+        infoWindow: { content: render_to_string(partial: "/burger_places/map_window", locals: { burger_place: burger_place }) }
       }
     end
   end
